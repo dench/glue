95fd3e2f0bd56a7376f3345412bb412880ad6c2b
Alexey Dench <dench@i.ua>
2020-06-15 11:13:58 +0300

    tz

--- views/category/index.php
+++ views/category/index.php
@@ -25,7 +25,7 @@ $this->params['breadcrumbs'][] = $page->name;
         <?php
         $url = Url::to((count($category->categories)) ? ['category/pod', 'slug' => $category->slug] : ['category/view', 'slug' => $category->slug]);
         ?>
-        <div class="col-sm-6 col-md-4 col-lg-3 pb-3 px-2">
+        <div class="col-sm-4 col-lg-3 pb-3 px-2">
             <div class="card block-link">
                 <a href="<?= $url ?>" rel="nofollow">
                 <?php if ($category->image) { ?>
