3f1588aea0e8df01f5b81bf1ef270931b83036b5
Alexey Dench <dench@i.ua>
2020-08-11 11:06:39 +0300

    Date in the reviews

--- views/site/_question_item.php
+++ views/site/_question_item.php
@@ -10,14 +10,14 @@
 ?>
 <div class="media-body p-3">
     <div class="mb-1">
-        <b><?= $model->name ?></b> | <span class="text-muted"><?= Yii::$app->formatter->asDate($model->created_at) ?></span>
+        <b><?= $model->name ?></b>, <span class="text-muted"><?= Yii::$app->formatter->asDate($model->created_at) ?></span>
     </div>
     <?= nl2br($model->question) ?>
 
     <?php if ($model->answer) : ?>
     <div class="media ml-5 p-2 mt-2 pb-0">
         <div class="media-body">
-            <div class="text-muted mb-1">Ответ компании | <span><?= Yii::$app->formatter->asDate($model->created_at) ?></span></div>
+            <div class="text-muted mb-1">Ответ компании, <span><?= Yii::$app->formatter->asDate($model->created_at) ?></span></div>
             <?= $model->answer ?>
         </div>
     </div>
