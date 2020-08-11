b27cc25010bbb4e7755dd4189174ac4a415040fe
Alexey Dench <dench@i.ua>
2020-08-11 11:03:30 +0300

    Date in the reviews

--- views/site/_review_item.php
+++ views/site/_review_item.php
@@ -18,15 +18,14 @@
             }
             ?>
         </div>
-        <b><?= $model->name ?></b>
-        <?php /*<span class="text-muted">Yii::$app->formatter->asDate($model->created_at)</span>*/ ?>
+        <b><?= $model->name ?></b> | <span class="text-muted"><?= Yii::$app->formatter->asDate($model->created_at) ?></span>
     </div>
     <?= nl2br($model->text) ?>
 
     <?php if ($model->answer) : ?>
-    <div class="media ml-5 p-2 mt-2">
+    <div class="media ml-5 p-2 mt-2 pb-0">
         <div class="media-body">
-            <div class="text-muted">Ответ компании</div>
+            <div class="text-muted mb-1">Ответ компании | <span><?= Yii::$app->formatter->asDate($model->created_at) ?></span></div>
             <?= $model->answer ?>
         </div>
     </div>
