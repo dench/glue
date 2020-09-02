434745f36fb1ee3f5847a84cba7eb8d9d13abd05
Alexey Dench <dench@i.ua>
2020-09-02 11:16:27 +0300

    ExchangeRate NBU

--- controllers/CronController.php
+++ controllers/CronController.php
@@ -8,49 +8,41 @@ use yii\web\Controller;
 
 class CronController extends Controller
 {
-    /**
-     * [0] => stdClass Object
-    (
-        [ccy] => USD
-        [base_ccy] => UAH
-        [buy] => 27.50000
-        [sale] => 27.90000
-    )
-
-    [1] => stdClass Object
-    (
-        [ccy] => EUR
-        [base_ccy] => UAH
-        [buy] => 32.30000
-        [sale] => 32.90000
-    )
-
-    [2] => stdClass Object
-    (
-        [ccy] => RUR
-        [base_ccy] => UAH
-        [buy] => 0.35900
-        [sale] => 0.38700
-    )
-
-    [3] =>
-        [ccy] => BTC
-        [base_ccy] => USD
-        [buy] => 10761.3847
-        [sale] => 11894.1621
-     */
     public function actionFinance()
     {
-        $json = file_get_contents('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11');
+        $json = file_get_contents('https://api.privatbank.ua/p24api/exchange_rates?json&date=' . date('d.m.Y'));
+        //$json = file_get_contents('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11');
 
         $data = json_decode($json);
 
         $currency = Currency::find()->where(['enabled' => 1])->orderBy(['position' => SORT_ASC])->one();
         $currencyDef = Currency::findOne(Yii::$app->params['currency_id']);
 
-        foreach ($data as $item) {
+        /*foreach ($data as $item) {
             if ($item->ccy == $currency->code) {
                 $currencyDef->rate = $item->sale;
+                $currencyDef->save();
+                break;
+            }
+        }*/
+
+        foreach ($data->exchangeRate as $item) {
+            if ($currency->code == @$item->currency && $rate = @$item->saleRateNB) {
+                $exchangeRateType = Yii::$app->params['exchangeRateType'];
+                switch ($exchangeRateType) {
+                    case 'NBU':
+                        $rate = @$item->saleRateNB;
+                        break;
+                    case 'PrivatSale':
+                        $rate = @$item->saleRate;
+                        break;
+                    case 'PrivatPurchase':
+                        $rate = @$item->purchaseRate;
+                        break;
+                    default:
+                        $rate = @$item->saleRateNB;
+                }
+                $currencyDef->rate = $rate;
                 $currencyDef->save();
                 break;
             }
